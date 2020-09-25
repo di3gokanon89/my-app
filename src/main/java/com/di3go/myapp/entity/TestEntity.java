1c720382c12a16991ea3efac17d078572a032d69
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2020-09-25 11:14:29 -0500

    Nombre del Proyecto: my-app, Descripción: versión final del proyecto my-app

---
+++ src/main/java/com/di3go/myapp/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.di3go.myapp.entity;
+
+/**
+ * @author Diego Paniagua Lopez.
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
